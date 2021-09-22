f80086685f8e9a2b34a129078ee58e7edcea3ddc
kavanak1 <k.kavana99@gmail.com>
2021-09-22 21:41:31 +0530

    writes flower program

---
+++ Flower.java
@@ -0,0 +1,26 @@
+class Flower{
+String name;
+String color;
+int noOfPetals;
+
+void bloom(){
+System.out.println(color+" "+name+" is blooming...!");
+}
+
+void shedFragrance(){
+System.out.println(color+" "+name+" is shedding fragrance...!");
+}
+
+public static void main(String[]args){
+
+ Flower rose= new Flower();
+
+rose.name="Rose";
+rose.color="yellow";
+rose.noOfPetals=16;
+rose.bloom();
+rose.shedFragrance();
+
+}
+
+}
