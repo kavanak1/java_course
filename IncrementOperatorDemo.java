41cc9f7bc2b89721636dded1039b64485aed9e9a
kavanak1 <k.kavana99@gmail.com>
2021-09-22 21:47:49 +0530

    writes IncrementOperatorDemo program

---
+++ IncrementOperatorDemo.java
@@ -0,0 +1,18 @@
+class IncrementOperatorDemo{
+
+public static void main(String[] args){
+
+//System.out.println("pre increment operation--------");
+    int a = 5;
+    int b = ++a;
+    System.out.println("a : "+a);
+    System.out.println("b : "+b);
+
+//System.out.println("post increment operation--------");
+    int p = 200;
+    int q = p++;
+    System.out.println("p : "+p);
+    System.out.println("q : "+q);
+
+}
+}
