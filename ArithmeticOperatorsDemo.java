2366b4463d39dfe1b68babf3536553d28074ce5c
kavanak1 <k.kavana99@gmail.com>
2021-09-22 21:44:56 +0530

    writes ArithmeticOperatorsDemo program

---
+++ ArithmeticOperatorsDemo.java
@@ -0,0 +1,35 @@
+class ArithmeticOperatorsDemo{
+
+public static void main(String[] args){
+
+int a=20;
+int b=10;
+int addResult = a+b;
+System.out.println("addResult : "+addResult);
+
+
+int x=50;
+int y=10;
+int subResult = x-y;
+System.out.println("subResult : "+subResult);
+
+int p=10;
+int q=10;
+int mulResult = p*q;
+System.out.println("mulResult : "+mulResult);
+
+int i=5;
+int j=10;
+int divResult = j/i;
+System.out.println("divResult : "+divResult);
+
+int m=9;
+int n=2;
+int modResult = m%n;
+System.out.println("modResult : "+modResult);
+
+}
+}
+
+
+
