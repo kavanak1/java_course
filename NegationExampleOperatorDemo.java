4ed05dce130e23c2bd5a671ef6e461d0bbc20a13
kavanak1 <k.kavana99@gmail.com>
2021-09-23 08:18:54 +0530

    writes NegationExampleOperatorDemo program

---
+++ NegationExampleOperatorDemo.java
@@ -0,0 +1,12 @@
+class NegationExampleOperatorDemo{
+
+public static void main(String[] args){
+
+    boolean b = true;
+    
+    boolean result = !b;
+ 
+    System.out.println("result : "+result);
+
+}
+}
