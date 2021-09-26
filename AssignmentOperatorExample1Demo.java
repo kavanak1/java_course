ef07c86602e496d1809ca3beb902238c69fcd023
kavanak1 <k.kavana99@gmail.com>
2021-09-26 22:32:41 +0530

    writes AssignmentOperatorExample1Demo program

---
+++ AssignmentOperatorExample1Demo.java
@@ -0,0 +1,14 @@
+class AssignmentOperatorExample1Demo
+{
+public static void main(String[] args)
+{
+int m = 100;
+int n = 150;
+n+=m;
+System.out.println("n : "+n);
+int i = 200;
+int j = 100;
+i/=j;
+System.out.println("i : "+i);
+}
+}
