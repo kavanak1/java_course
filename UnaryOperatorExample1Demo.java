556acab9666364b173923ce4ff9af9faba39bd56
kavanak1 <k.kavana99@gmail.com>
2021-09-26 22:26:32 +0530

    writes UnaryOperatorExample1Demo program

---
+++ UnaryOperatorExample1Demo.java
@@ -0,0 +1,14 @@
+class UnaryOperatorExample1Demo
+{
+public static void main(String[] args)
+{
+int i = 10;
+i++;
+System.out.println(i);
+++i;
+System.out.println(i);
+System.out.println(++i);
+System.out.println(i++);
+System.out.println(i);
+}
+}
