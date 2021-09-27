6263d989fb6e2343df71ea9dd50b5d7f9a17bc98
kavanak1 <k.kavana99@gmail.com>
2021-09-27 12:14:36 +0530

    writes TernaryOperatorDemo program

---
+++ TernaryOperatorDemo.java
@@ -0,0 +1,13 @@
+class TernaryOperatorDemo
+{
+public static void main(String[] args)
+{
+int x=100;
+int y=200;
+int result=x>y?x:y;
+System.out.println("Largest of" + x + "&" +y+" is: "+result);
+int result1=x<y?x:y;
+System.out.println("Smallest of" + x + "&" +y+" is: "+result1);
+}
+}
+
