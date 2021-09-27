b3b8640dd153d25f75e66beab4817de0d6987e12
kavanak1 <k.kavana99@gmail.com>
2021-09-27 08:49:20 +0530

    writes LogicalOperatorExampleDemo program

---
+++ LogicalOperatorExampleDemo.java
@@ -0,0 +1,12 @@
+class LogicalOperatorExampleDemo
+{
+public static void main(String[] args)
+{
+int a = 50;
+int b = 80;
+System.out.println(a<b && a<=b);
+System.out.println(a<b && a>b);
+System.out.println(a==b || a!=b);
+System.out.println(a==b || a>b);
+}
+}
